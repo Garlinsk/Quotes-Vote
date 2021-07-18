21942f17418efa8a5f4287865d9b107e9ba451ea
franklin <franklinwanami@gmail.com>
2021-07-19 00:01:10 +0300

    added modified src/app/details/details.component.ts and src/app/form/form.component.html

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  @Input() quote:Quote
 
   constructor() { }
 
